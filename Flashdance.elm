c9984d9c9ed17b9de3fac73b6a365d7e00843ec0
Tim Habermaas <tim.habermaas@gmail.com>
2015-04-03 02:10:51 +0200

    [WIP] svg stuff

--- Flashdance.elm
+++ Flashdance.elm
@@ -1,8 +1,10 @@
 module Flashdance where
 
-import Html (..)
-import Html.Attributes (..)
-import Html.Events (..)
+import Html as H
+import Html.Attributes as HA
+import Html.Events as HE
+import Svg as S
+import Svg.Attributes as SA
 import Signal
 import List
 import Text
@@ -22,8 +24,9 @@ type Effect = AjaxRequest String
 type SeatState = Reserved | Unusable | Free
 type Selected = Selected | Unselected
 
-type alias Seat = { pos: (Int,Int), row: Int, number: Int, selected: Selected, state: SeatState }
-type alias Stand = List Seat
+type alias Seat = { x: Int, number: Int, selected: Selected, state: SeatState }
+type alias Row = { number: Int, y: Int, seats: List Seat }
+type alias Stand = List Row
 
 
 toggleSelected : Seat -> Seat
@@ -34,7 +37,7 @@ toggleSelected s = case s.selected of
 updateStand : Action -> Stand -> Stand
 updateStand action stand =
   case action of
-    ClickSeat seat -> List.map (\s -> if s == seat then toggleSelected s else s) stand
+    ClickSeat seat -> stand
     _ -> stand
 
 update : Action -> (Model, Maybe Effect) -> (Model, Maybe Effect)
@@ -65,26 +68,19 @@ toRequest e = case e of
 fooRequest : Signal String
 fooRequest = Signal.map (snd >> toRequest) updatesWithEffect |> Signal.dropIf String.isEmpty ""
 
-row : Int -> List Seat -> List Seat
-row n = List.map (\s -> { s | row <- n, pos <- (fst s.pos, (snd s.pos) + n) })
-
-buildSeat : Int -> Seat
-buildSeat n = {pos = (n,0), row = 16, number = n, selected = Unselected, state = Free}
+dummyRow : List Seat
+dummyRow = List.map (\n -> {x = n, number = n, selected = Unselected, state = Free}) [1..100]
 
 dummyStand : Stand
-dummyStand = List.foldl (\c acc -> acc ++ (row c <| List.map buildSeat [0..19])) [] [1..18]
+dummyStand = List.map (\n -> { number = 17 - n, y = n, seats = dummyRow }) [1..30]
 
-view : Model -> Html
+view : Model -> H.Html
 view model =
-  div [ class "container" ]
-    [ a [ class "btn btn-primary btn-small", href "#", onClick (Signal.send actionChannel CountUp) ] [ text "+" ],
-      a [ class "btn btn-primary btn-small", href "#", onClick (Signal.send actionChannel CountDown) ] [ text "-" ],
-      text (toString model.counter),
-      text model.title,
-      div [] [
-        fromElement (drawStand model.seats)
+    H.div []
+      [ drawStand model.seats
+      , H.text <| toString model.counter
+      , H.button [ HE.onClick (Signal.send actionChannel CountUp) ] [H.text "fo"]
       ]
-    ]
 
 scaleTuple : (Int,Int) -> Float -> (Float,Float)
 scaleTuple (x,y) s = ((toFloat x) * s, (toFloat y) * s)
@@ -96,22 +92,8 @@ seatShape s = case s.state of
   Unusable -> outlined (solid black)
   Free -> if (s.selected == Selected) then outlined (solid red) else outlined (solid black)
 
-drawSeat : Seat -> Form
-drawSeat seat = move (scaleTuple seat.pos 21) <|
-                toForm <|
-                clickable (Signal.send actionChannel (ClickSeat seat)) <|
-                collage 20 20
-                  [ group
-                    [ (seatShape seat) (rect 20.0 20.0),
-                      toForm <| Text.centered <| Text.fromString <| toString seat.number
-                    ]
-                  ]
 
-
-drawStand : Stand -> Element
-drawStand stand = collage 1000 400 <| List.map drawSeat stand
-
-main : Signal Html
+main : Signal H.Html
 main =
   Signal.map view model
 
@@ -133,3 +115,18 @@ model =
 actionChannel : Signal.Channel Action
 actionChannel =
   Signal.channel CountUp
+
+
+-- SVG FOO
+
+drawSeat : Seat -> S.Svg
+drawSeat seat = S.g [HE.onClick (Signal.send actionChannel CountUp), SA.transform <| "translate(" ++ (toString <| seat.x * 18) ++ ", 0)"]
+                  [ S.rect [SA.style "fill:rgba(0,0,0,0);stroke-width: 1; stroke: rgb(0, 0, 0)", SA.width "18", SA.height "18"] []
+                  , S.text [SA.x "9", SA.y "14", SA.textAnchor "middle"] [ H.text <| toString seat.number]
+                  ]
+
+drawRow : Row -> S.Svg
+drawRow row = S.g [SA.transform <| "translate(0," ++ (toString <| row.y * 18) ++ ")"] (List.map drawSeat row.seats)
+
+drawStand : Stand -> H.Html
+drawStand stand = S.svg [SA.version "1.1", SA.x "0", SA.y "0", SA.height "500", SA.width "1000"] <| List.map drawRow stand
